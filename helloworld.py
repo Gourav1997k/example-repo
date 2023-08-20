baa9be6465cddc50a0e4c8f9c8c492e2c7fc13cb
Gourab Karmakar <74773862+Gourav1997k@users.noreply.github.com>
2023-08-20 11:24:40 +0530

    Update helloworld.py
    removed the merge conflicts

--- helloworld.py
+++ helloworld.py
@@ -1,12 +1,8 @@
 print("hello world!!")
 
-<<<<<<< HEAD
 n = int(input("Enter the number: "))
 
 if n%2==0:
     print("Even number")
 else:
     print("Odd number")
-=======
-n = int(input("Enter the number: "))
->>>>>>> main
